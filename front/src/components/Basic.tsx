105569afcbf757a7059c5feb08e8dea719e4ffe2
togami <tuabobo123@gmail.com>
2020-06-14 22:41:29 +0900

    add redux

---
+++ front/src/components/Basic.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TextField } from "@material-ui/core";
+
+import useStyles from "./styles";
+import { RootState } from "../domain/entity/rootState";
+
+import { Profile } from "../domain/entity/profile";
+import { PROFILE } from "../domain/services/profile";
+import profileActions from "../store/profile/actions";
+
+const Basic: React.SFC = () => {
+  const dispatch = useDispatch();
+  const profile = useSelector((state: RootState) => state.profile);
+
+  const handleChange = (member: Partial<Profile>) => {
+    dispatch(profileActions.setProfile(member));
+  };
+  const classes = useStyles();
+  return (
+    <>
+      <TextField
+        fullWidth
+        className={classes.formField}
+        label={PROFILE.NAME}
+        value={profile.name}
+        onChange={(e) => handleChange({ name: e.target.value })}
+      />
+      <TextField
+        fullWidth
+        multiline
+        className={classes.formField}
+        rows={5}
+        label={PROFILE.DESCRIPTION}
+        value={profile.description}
+        onChange={(e) => handleChange({ description: e.target.value })}
+      />
+    </>
+  );
+};
+
+export default Basic;
