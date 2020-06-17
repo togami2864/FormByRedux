a3442618341ac9f1dbee21492d27e5dea85d00ad
togami <tuabobo123@gmail.com>
2020-06-17 18:24:29 +0900

    add validation

--- front/src/store/index.tsx
+++ front/src/store/index.tsx
@@ -2,12 +2,14 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import profileReducer from "./profile/reducer";
 import collegesReducer from "./colleges/reducer";
+import validationReducer from "./validation/reducer";
 import { RootState } from "../domain/entity/rootState";
 
 const store = createStore(
   combineReducers<RootState>({
     profile: profileReducer,
     colleges: collegesReducer,
+    validation: validationReducer,
   }),
   compose(
     applyMiddleware(thunk),
